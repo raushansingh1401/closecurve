d5bfcea68307e033d1f99ac3252a400972976dd4
raushansingh1401 <raushansingh1401@gmail.com>
2025-06-01 07:38:08 +0530

    WIP : Deployement of application

--- backend/app.py
+++ backend/app.py
@@ -52,4 +52,7 @@ def average_close():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
